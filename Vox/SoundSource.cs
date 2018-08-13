52b6aac3eb29744a3db560ea0c63abddc59ba3bd
Adel Vilkov <vilkov.adel@gmail.com>
2018-08-13 18:45:07 +0300

    WIP Some more work on SoundSource

--- Vox/SoundSource.cs
+++ Vox/SoundSource.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Numerics;
 using static OpenAL.AL10;
 using static Vox.ErrorHandler;
 using static Vox.Internal.Util;
@@ -53,6 +55,259 @@ namespace Vox
                         "alGetSourcei(AL_SOURCE_STATE)"));
                 return (SourceState)result;
             }
+        }
+
+        private bool _isRelative = false;
+        /// <summary>
+        /// Gets or sets if the source properties should be interpreted as relative
+        /// to the listener's position.
+        /// </summary>
+        public bool IsRelative
+        {
+            get => _isRelative;
+            set {
+                UseDevice(Owner, () =>
+                    AL(() => alSourcei(_handle, AL_SOURCE_RELATIVE, value ? 1 : 0),
+                        "alSourcei(AL_SOURCE_RELATIVE)"));
+                _isRelative = value;
+            }
+        }
+
+        private bool _isLooping = false;
+        /// <summary>
+        /// Gets or sets if the source is looped.
+        /// </summary>
+        public bool IsLooping
+        {
+            get => _isLooping;
+            set {
+                UseDevice(Owner, () =>
+                    AL(() => alSourcei(_handle, AL_LOOPING, value ? 1 : 0),
+                        "alSourcei(AL_LOOPING)"));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the first buffer on the queue.
+        /// </summary>
+        /// <remarks>If set to null, releases the current buffer queue (only in Initial and Stopped states).</remarks>
+        public SoundBuffer CurrentBuffer
+        {
+            set {
+                if (value == null) {
+                    switch (State)
+                    {
+                        case SourceState.Initial:
+                        case SourceState.Stopped:
+                            UseDevice(Owner, () => 
+                                AL(() => alSourcei(_handle, AL_BUFFER, AL_NONE),
+                                    "alSourcei(AL_BUFFER)"));
+                            return;
+                        default:
+                            throw new AudioLibraryException(
+                                "Could not empty queue on an active sound source");
+                    }
+                }
+                UseDevice(Owner, () =>
+                    AL(() => alSourcei(_handle, AL_BUFFER, (int)value._bufferId),
+                        "alSourcei(AL_BUFFER)"));
+            }
+            get {
+                var bufferId = AL_NONE;
+                UseDevice(Owner, () =>
+                    AL(() => alGetSourcei(_handle, AL_BUFFER, out bufferId),
+                        "alGetsourcei(AL_BUFFER)"));
+                if (bufferId == AL_NONE) return null;
+                return Owner._resources
+                    .OfType<SoundBuffer>()
+                    .FirstOrDefault(buf => buf._bufferId == bufferId);
+            }
+        }
+
+        /// <summary>
+        /// Returns the total number of buffers in a queue (not played, currently
+        /// playing and played already).
+        /// </summary>
+        public int QueuedBuffers
+        {
+            get {
+                var result = 0;
+                UseDevice(Owner, () =>
+                    AL(() => alGetSourcei(_handle, AL_BUFFERS_QUEUED, out result),
+                        "alGetSourcei(AL_BUFFERS_QUEUED)"));
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of buffers that have been played by this source.
+        /// </summary>
+        public int ProcessedBuffers
+        {
+            get {
+                var result = 0;
+                UseDevice(Owner, () =>
+                    AL(() => alGetSourcei(_handle, AL_BUFFERS_PROCESSED, out result),
+                        "alGetSourcei(AL_BUFFERS_PROCESSED)"));
+                return result;
+            }
+        }
+
+        private float _minGain = 0.0f;
+        /// <summary>
+        /// Gets or sets the minimum guaranteed gain for the source ranging
+        /// from 0.0 to 1.0.
+        /// </summary>
+        public float MinGain
+        {
+            get => _minGain;
+            set {
+                var val = Math.Max(0.0f, Math.Min(1.0f, value));
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_MIN_GAIN, val),
+                        "alSourcef(AL_MIN_GAIN)"));
+                _minGain = val;
+            }
+        }
+
+        private float _maxGain = 0.0f;
+        /// <summary>
+        /// Gets or sets the maximum gain for the source ranging
+        /// from 0.0 to 1.0. Setting it to 0.0 mutes the source.
+        /// </summary>
+        public float MaxGain
+        {
+            get => _maxGain;
+            set {
+                var val = Math.Max(0.0f, Math.Min(1.0f, value));
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_MAX_GAIN, val),
+                        "alSourcef(AL_MAX_GAIN)"));
+                _maxGain = val;
+            }
+        }
+
+        private float _referenceDistance = 1.0f;
+        /// <summary>
+        /// Gets or sets the reference distance used in attenuation calculations.
+        /// </summary>
+        public float ReferenceDistance
+        {
+            get => _referenceDistance;
+            set
+            {
+                var val = Math.Min(0.0f, value);
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_REFERENCE_DISTANCE, val),
+                        "alSourcef(AL_REFERENCE_DISTANCE)"));
+                _referenceDistance = val;
+            }
+        }
+
+        private float _rolloffFactor = 1.0f;
+        /// <summary>
+        /// Gets or sets the rolloff factor used in attenuation calculations.
+        /// </summary>
+        public float RolloffFactor
+        {
+            get => _rolloffFactor;
+            set 
+            {
+                var val = Math.Min(0.0f, value);
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_ROLLOFF_FACTOR, val),
+                        "alSourcef(AL_ROLLOFF_FACTOR)"));
+                _rolloffFactor = val;
+            }
+        }
+
+        private float _maxDistance = float.MaxValue;
+        /// <summary>
+        /// Gets or sets the maximum distance parameter for the
+        /// 'Inverse Clamped Distance' attenuation model.
+        /// </summary>
+        public float MaxDistance
+        {
+            get => _maxDistance;
+            set {
+                var val = Math.Min(0.0f, value);
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_MAX_DISTANCE, val),
+                        "alSourcef(AL_MAX_DISTANCE)"));
+                _maxDistance = val;
+            }
+        }
+
+        private float _pitch = 1.0f;
+        /// <summary>
+        /// Gets or sets the desired pitch shift, where 1.0 equals identity.
+        /// Each reduction by 50% equals a pitch shift of -12 semitones (one octave),
+        /// each doubling equals a pitch shift of +12 semitones (one octave).
+        /// Must be greater than zero.
+        /// </summary>
+        public float Pitch
+        {
+            get => _pitch;
+            set {
+                var val = Math.Min(float.Epsilon, value);
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_PITCH, val),
+                        "alSourcef(AL_PITCH)"));
+                _pitch = val;
+            }
+        }
+
+        private Vector3 _direction = Vector3.Zero;
+        /// <summary>
+        /// Gets or sets the direction of the sound source. If direction is a
+        /// zero vector, the source is omnidirectional.
+        /// </summary>
+        public Vector3 Direction
+        {
+            get => _direction;
+            set {
+                UseDevice(Owner, () =>
+                    AL(() => alSource3f(_handle, AL_DIRECTION, value.X, value.Y, value.Z),
+                        "alSource3f(AL_DIRECTION)"));
+                _direction = value;
+            }
+        }
+
+        private float _innerAngle = 360.0f;
+        /// <summary>
+        /// Inside angle of the sound cone, in degrees. Default value of 360 means
+        /// that the source is omnidirectional.
+        /// </summary>
+        public float InnerAngle
+        {
+            get => _innerAngle;
+            set {
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_CONE_INNER_ANGLE, value),
+                        "alSource3f(AL_CONE_INNER_ANGLE)"));
+                _innerAngle = value;
+            }
+        }
+
+        private float _outerAngle = 360.0f;
+        /// <summary>
+        /// Outer angle of the sound cone, in degrees.
+        /// </summary>
+        public float OuterAngle
+        {
+            get => _outerAngle;
+            set {
+                UseDevice(Owner, () =>
+                    AL(() => alSourcef(_handle, AL_CONE_OUTER_ANGLE, value),
+                        "alSource3f(AL_CONE_OUTER_ANGLE)"));
+                _outerAngle = value;
+            }
+        }
+
+        private float _coneOuterGain = 0.0f;
+        public float OuterGain
+        {
+            get => _coneOuterGain;
         }
 
         /// <summary>
